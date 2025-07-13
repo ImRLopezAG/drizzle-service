cb23c2a7903b4d6abe8ad9aa74aaac49a2074c70
ImRLopezAG <angelg00lopez@gmail.com>
2025-07-13 19:43:55 -0400

    feat: add @vercel/react-router as a dependency and integrate vercelPreset in router config

--- apps/docs/react-router.config.ts
+++ apps/docs/react-router.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from '@react-router/dev/config';
 import { source } from './src/app/source';
+import { vercelPreset } from '@vercel/react-router/vite';
 
 export default {
   ssr: true,
@@ -7,4 +8,5 @@ export default {
     return [...getStaticPaths(), ...source.getPages().map((page) => page.url)];
   },
   appDirectory: 'src/app',
+  presets: [vercelPreset()],
 } satisfies Config;
